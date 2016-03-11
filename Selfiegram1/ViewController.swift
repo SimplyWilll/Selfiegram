f05c2c14d1fe1aa526c08ea651e9d5331aadb42b
rajgithub7 <supercomputer@supers-macbook-pro.local>
2016-03-10 22:31:57 -0800

    Initial Commit

---
+++ Selfiegram1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfiegram1
+//
+//  Created by Super Computer on 2016-03-10.
+//  Copyright © 2016 Super Computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
